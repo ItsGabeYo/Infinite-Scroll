5d479dbe9b4148dc8fdebda52140510a2ab07439
draghn <g.vaSile@gmail.com>
2020-08-17 16:50:24 -0700

    Refactoring

--- script.js
+++ script.js
@@ -16,19 +16,30 @@ const getPhotos = async () => {
   }
 };
 
+// Helper Function to Set Attributes on DOM Elements
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
 // Create Elements for Links & Photos, Add to DOM
 const displayPhotos = (photosArray) => {
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // Create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
